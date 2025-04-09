e7f45482fdeeb112d5901d21c26d1735043523ad
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-04-09 08:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+
+
+const output = document.getElementById("output");
+  const startTime = performance.now();
+
+
+  function createPromise() {
+    const time = Math.random() * 2 + 1; 
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(time.toFixed(3));
+      }, time * 1000);
+    });
+  }
+
+
+  const promises = [createPromise(), createPromise(), createPromise()];
+
+  Promise.all(promises).then(times => {
+    const totalTime = ((performance.now() - startTime) / 1000).toFixed(3);
+    output.innerHTML = ""; 
+
+ 
+    times.forEach((time, index) => {
+      output.innerHTML += `<tr><td>Promise ${index + 1}</td><td>${time}</td></tr>`;
+    });
+
+
+    output.innerHTML += `<tr><td>Total</td><td>${totalTime}</td></tr>`;
+  });
