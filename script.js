ec37f26a34bba0874d7a8fd15b3111cdbac2254a
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-04-09 09:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-\const output = document.getElementById("output");
+\<script>
+  const output = document.getElementById("output");
 
-function createPromise(fixedTime) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(fixedTime.toFixed(3));
-    }, fixedTime * 1000);
+  function createPromise(fixedTime) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(fixedTime.toFixed(3));
+      }, fixedTime * 1000);
+    });
+  }
+
+  const promises = [
+    createPromise(2),
+    createPromise(1),
+    createPromise(3)
+  ];
+
+  Promise.all(promises).then(times => {
+    output.innerHTML = "";
+
+    times.forEach((time, index) => {
+      output.innerHTML += `<tr><td>Promise ${index + 1}</td><td>${time}</td></tr>`;
+    });
+
+    const totalTime = Math.max(...times.map(Number)).toFixed(3);
+    output.innerHTML += `<tr><td>Total</td><td>${totalTime}</td></tr>`;
   });
-}
-
-const promises = [
-  createPromise(2),
-  createPromise(1),
-  createPromise(3)
-];
-
-Promise.all(promises).then(times => {
-  output.innerHTML = "";
-
-  times.forEach((time, index) => {
-    output.innerHTML += `<tr><td>Promise ${index + 1}</td><td>${time}</td></tr>`;
-  });
-
-  const totalTime = Math.max(...times.map(Number)).toFixed(3); // if required by test
-  output.innerHTML += `<tr><td>Total</td><td>${totalTime}</td></tr>`;
-});
+</script>
